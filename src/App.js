294e963791e1ca0a6c562937169cd74ace8a989c
Jedediah Arnold <jarnold910@gmail.com>
2021-01-04 20:36:54 -0500

    start the blog files and main design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container, Paper, Typography, Button } from '@material-ui/core'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="md">
+        <Paper elevation={3} mt={5}>
+          <Typography variant="h2" component="h1">Microblog</Typography>
+          <Typography variant="subtitle1" component="p">Get in the Springboard of blogging!</Typography>
+          <Button color="primary">Blog</Button>
+          <Button color="primary">Add a Post</Button>
+        </Paper>
+      </Container>
     </div>
   );
 }
